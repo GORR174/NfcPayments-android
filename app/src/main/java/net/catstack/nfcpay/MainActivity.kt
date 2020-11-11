3f46c62c91f2ce4cf844afe6a9e13191b7db77cf
Gorr <gorrgorrob@gmail.com>
2020-11-11 16:31:58 +0500

    Navigation drawer

--- app/src/main/java/net/catstack/nfcpay/MainActivity.kt
+++ app/src/main/java/net/catstack/nfcpay/MainActivity.kt
@@ -3,9 +3,11 @@ package net.catstack.nfcpay
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +17,7 @@ import org.koin.android.ext.android.inject
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private val accountRepository: AccountRepository by inject()
 
@@ -22,13 +25,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.navHostFragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_history, R.id.navigation_profile))
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_history, R.id.navigation_profile)
+            .setOpenableLayout(drawerLayout)
+            .build()
+        setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navigationDrawer.menu.add("Customer").setIcon(R.drawable.ic_profile).setOnMenuItemClickListener {
+            return@setOnMenuItemClickListener true
+        }
+        navigationDrawer.menu.add("АО «Альфа-Банк»").setIcon(R.drawable.ic_home).setOnMenuItemClickListener {
+            return@setOnMenuItemClickListener true
+        }
+        navigationDrawer.menu.add("ООО «Моя оборона»").setIcon(R.drawable.ic_home).setOnMenuItemClickListener {
+            return@setOnMenuItemClickListener true
+        }
 
         if (!accountRepository.isUserAuthorized()) {
             navController.navigate(MobileNavigationDirections.actionGlobalLoginFragment())
@@ -48,6 +63,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
